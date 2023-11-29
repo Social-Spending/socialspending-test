9aeceeb945fa58e960ca9205aaff19a340b1652a
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-11-28 20:56:15 -0500

    Remove redundant imports

--- python/SearchUsers/SearchUserTestMgr.py
+++ python/SearchUsers/SearchUserTestMgr.py
@@ -15,23 +15,6 @@ from constants import *
 from TestMgrBase import TestMgrBase
 import requests
 
-###############################################################
-# This makes it possible to import TestMgrBase and constants.py
-###############################################################
-import sys
-from os.path import dirname, abspath
-
-# Get parent directory of current file (should be `/path/to/socialspendingapp-tests/python`)
-parent_dir = dirname(dirname(abspath(__file__)))
-
-# Append to sys.path to be able to import TestMgrBase and constants
-sys.path.append(parent_dir)
-###############################################################
-
-from constants import *
-from TestMgrBase import TestMgrBase
-import requests
-
 class SearchUsersTestMgr(TestMgrBase):
     def setup(self):
         self.test_name = "SearchUsersTestMgr"
